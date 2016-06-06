828caa7a4ccc7301f9afe44eeb99a46282b13612
Eric Thivierge <ethivierge@gmail.com>
2016-06-06 12:08:29 -0400

    Putting lowdown back into setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ from distutils.core import setup
 # Requirements.
 setup_requires = [
     'mock >= 1.3, <2',
-    'sphinx >= 1.3, < 2'
+    'sphinx >= 1.3, < 2',
+    'lowdown'
 ]
 
 install_requires = []
